ae7dffe7d2edad52511469ec1d5e0ca953373b2a
imaginennn <imaginennn@users.noreply.github.com>
2025-05-21 21:12:02 +0200

    Add connection string and service in program.cs

--- MoviesAnalysisApp/Program.cs
+++ MoviesAnalysisApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MoviesAnalysisApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Add database context
+builder.Services.AddDbContext<MoviesAnalysisAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
